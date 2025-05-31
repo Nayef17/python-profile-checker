ddf9c4e4c554bb664aa454b1c99eb3632d4ef688
Nayef <nayefhaidar050@gmail.com>
2025-05-31 23:50:23 +0300

    modification

--- python-profile-checker.py
+++ python-profile-checker.py
@@ -1,12 +1,12 @@
-name = input("Name: ")
+# name = input("Name: ")
 Age = int(input("Age: "))
-GPA = float(input("Enter your GPA (0 to 5): "))
-FOI = str((input("Enter your Field of Interest: ")))
+GPA = float(int(input("Enter your GPA (0 to 5): ")))
+# FOI = str((input("Enter your Field of Interest: ")))
 graduated = str(input("Graduated? YES or NO: ")).strip().lower()
 
 if Age < 25 and GPA >= 3.5 and graduated == "yes":
     print("You are eligible for the scholarship")
-elif Age < 30 or GPA >= 2.5:
+elif Age < 30 and GPA >= 2.5:
     print("You are eligible for the Internship")
 else:
     print("You are not eligible for any of the programs")
