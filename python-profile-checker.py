d5f03709cd5c899354612962ba5e46d6a7392338
Nayef <nayefhaidar050@gmail.com>
2025-05-31 23:59:17 +0300

    removing comment

--- python-profile-checker.py
+++ python-profile-checker.py
@@ -1,7 +1,7 @@
-# name = input("Name: ")
+name = input("Name: ")
 Age = int(input("Age: "))
 GPA = float(int(input("Enter your GPA (0 to 5): ")))
-# FOI = str((input("Enter your Field of Interest: ")))
+FOI = str((input("Enter your Field of Interest: ")))
 graduated = str(input("Graduated? YES or NO: ")).strip().lower()
 
 if Age < 25 and GPA >= 3.5 and graduated == "yes":
